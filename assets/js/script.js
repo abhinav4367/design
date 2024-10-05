dbab83a4b2a8e89477bb07b6ea746846c417bd37
Abhinav M <abhinav4367@gmail.com>
2024-10-05 08:50:16 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,16 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
+// function to download file directly
+function downloadFile(filename, content) {
+  const blob = new Blob([content], { type: 'text/plain' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
